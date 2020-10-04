febc6deb5f6bdb7bf6b6f0d7c4d5e354567df7e2
Shreyas Chavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-10-04 16:25:37 +0530

    Add files via upload

---
+++ Day 003 - 4th October 2020/Hackeerrank_CPP_Practice_Basic_Data_Type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char ch;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf", &a, &b, &ch, &d, &e);
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", ch);
+    printf("%f\n", d);
+    printf("%lf\n", e);
+
+    return 0;
+}
