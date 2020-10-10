6018204faa8d855b370687079e775479cc8e974d
Shreyas Chavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-10-10 15:27:43 +0530

    Add files via upload

---
+++ Day 009 - 10th October 2020/variable-comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// I have started learning this series from 10th october 2020
+/*
+Name - Shreyas
+Date of Starting - 10th October 2020
+*/
+
+int main(){
+  int sum = 9;
+  cout << "Hello World! "<< sum;
+  return 0;
+}
