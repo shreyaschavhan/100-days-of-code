fd0e5e647a46bb04e1f446dc6c2dd813d5753a8b
Shreyas Chavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-10-12 15:08:02 +0530

    Add files via upload

--- Day 011 - 12th October 2020/control-structures-if-else-statement-and-switch-cases.cpp
+++ Day 011 - 12th October 2020/control-structures-if-else-statement-and-switch-cases.cpp
@@ -61,5 +61,30 @@ int main(){
     }
 
     cout<<"Done with switch case";
+
+    cout << "----------------------Loop Control Structure-----------------------" << endl;
+    cout << "There are 3 types of loops: " << endl;
+    cout << "\t1. for-loop" << endl;
+    cout << "\t2. while-loop" << endl;
+    cout << "\t3. do-whlie-loop" << endl;
+
+    cout << "----------For Loop------------" << endl;
+    for (int i = 0; i < 100; i++){
+      cout << i << endl;
+    }
+
+    cout << "----------While Loop------------" << endl;
+    int i = 0;
+    while(i < 100){
+      cout << i << endl;
+      i++;
+    }
+    cout << "----------do While Loop------------" << endl;
+    int j = 1;
+    do{
+    cout << j <<endl;
+    j += 1;
+  }while(j <= 100);
+
     return 0;
 }
