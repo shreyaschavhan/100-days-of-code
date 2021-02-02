a28dd421e01f45fe372e3098cd0de7cc853ddf72
Shreyas Chavhan <68887544+shreyaschavhan@users.noreply.github.com>
2021-02-02 17:05:39 +0530

    Add files via upload

--- Day 121 - 30th January 2021/trial.cpp
+++ Day 121 - 30th January 2021/trial.cpp
@@ -5,22 +5,21 @@
 using namespace std;
 
 int main(){
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
     int T;
     cin >> T;
-    while(T--){
-        int N;
-        cin >> N;
-        int S, V, P;
-        int maxProfit = 0;
-        for(int i = 0; i < N; i++){
-            cin >> S >> V >> P;
-            maxProfit = max(maxProfit, ((P/(S+1)) * V));
+    while (T--){
+        int n;
+        cin >> n;
+        int s, p, v;
+        int maxProfit=0;
+        for ( int i=0; i<n; i++){
+            cin >> s>> p>> v;
+            s++;
+            maxProfit = max(maxProfit, p/s * v);
+
         }
-        cout << maxProfit << '\n';
+     cout << maxProfit <<endl;
     }
-    return 0;
+	return 0;
 }
